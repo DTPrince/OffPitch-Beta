64d54d7515c619ac96a1912b5e0f44c6d33fefff
Derek Prince <ace.prince08@gmail.com>
2018-03-19 16:09:52 -0600

    Irrelevant commit to get my pulls to avoid merging

--- peripheral_interfaces.h
+++ peripheral_interfaces.h
@@ -4,7 +4,6 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-//////////////////// PERIPH
 uint8_t enable_stepper(int port, int pin);
 
 uint8_t disable_stepper(int port, int pin);
@@ -13,7 +12,6 @@ uint8_t set_stepperDirection(int port, int pin, bool direction);
 
 uint8_t set_stepperSpeed(int port, int pin, int speed);
 
-// probs have to change the return and argument types after looking through documentation
 uint8_t get_DIOPinState(int port, int pin);
 
 uint16_t get_ADCValue(int port, int pin);
